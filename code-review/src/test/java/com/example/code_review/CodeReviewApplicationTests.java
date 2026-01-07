063e87b603e8d1f39d7e0bf35d915eaddc29ec2f
aat <learnedsageateudaimonia@gmail.com>
2026-01-07 11:58:55 +0530

    Add test

---
+++ code-review/src/test/java/com/example/code_review/CodeReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.code_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
