9cf855caa0dd7457975520b4b3f0049755a41eae
aat <learnedsageateudaimonia@gmail.com>
2026-01-07 11:58:05 +0530

    Add error response DTO

---
+++ code-review/src/main/java/com/aat_projects/code_review/api/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.aat_projects.code_review.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ErrorResponse {
+    private String code;
+    private String message;
+}
